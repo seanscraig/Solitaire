807cf7860cf914d7cc0979fb925f024c9d42a561
Sean Craig <seancraig21@gmail.com>
2020-12-23 15:13:53 -0600

    Added Shuffle

--- Assets/Solitaire.cs
+++ Assets/Solitaire.cs
@@ -24,6 +24,7 @@ public class Solitaire : MonoBehaviour
     public void PlayCards()
     {
         deck = GenerateDeck();
+        Shuffle(deck);
 
         //test
         foreach (string card in deck)
@@ -45,5 +46,19 @@ public class Solitaire : MonoBehaviour
         }
 
         return newDeck;
+    }
+
+    void Shuffle<T>(List<T> list)
+    {
+        System.Random random = new System.Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            int k = random.Next(n);
+            n--;
+            T temp = list[k];
+            list[k] = list[n];
+            list[n] = temp;
+        }
     }
 }
